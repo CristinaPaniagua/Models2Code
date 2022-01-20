8a4df4309d7bac6a57cc62f88e6d04d735ae89cd
CristinaPaniagua <cristina.paniagua@ltu.se>
2022-01-20 15:08:10 +0100

    update to AH framework version 4.4.0.2

--- eu.arrowhead.skelettons.deployment/bin/resources/folderGen.bat
+++ eu.arrowhead.skelettons.deployment/bin/resources/folderGen.bat
@@ -1,11 +1,11 @@
 D:
 cd D:\SysMLPlugins\TestDirectory
-mkdir test1_ApplicationSystems
-cd test1_ApplicationSystems
+mkdir testclass206_ApplicationSystems
+cd testclass206_ApplicationSystems
 mkdir indoorTemp_Provider
 mkdir outdoorTemp_Provider
 mkdir controlSystem_Provider
 mkdir boiler_Consumer
 mkdir iHM_Provider
 
-copy D:\SysMLPlugins\Code\eu.arrowhead.skelettons.deployment\src\resources\.project D:\SysMLPlugins\TestDirectory\test1_ApplicationSystems
+copy D:\SysMLPlugins\Code\eu.arrowhead.skelettons.deployment\src\resources\.project D:\SysMLPlugins\TestDirectory\testclass206_ApplicationSystems
--- eu.arrowhead.skelettons.deployment/src/resources/folderGen.bat
+++ eu.arrowhead.skelettons.deployment/src/resources/folderGen.bat
@@ -1,7 +1,11 @@
 D:
 cd D:\SysMLPlugins\TestDirectory
-mkdir test4_ApplicationSystems
-cd test4_ApplicationSystems
+mkdir test_ApplicationSystems
+cd test_ApplicationSystems
+mkdir indoorTemp_Provider
+mkdir outdoorTemp_Provider
 mkdir controlSystem_Provider
+mkdir boiler_Consumer
+mkdir iHM_Provider
 
-copy D:\SysMLPlugins\Code\eu.arrowhead.skelettons.deployment\src\resources\.project D:\SysMLPlugins\TestDirectory\test4_ApplicationSystems
+copy D:\SysMLPlugins\Code\eu.arrowhead.skelettons.deployment\src\resources\.project D:\SysMLPlugins\TestDirectory\test_ApplicationSystems
