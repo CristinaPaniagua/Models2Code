6819a5857285d63665dc9980e4676b4ecbce75af
Fernando Labra Caso <fernandolabracaso@gmail.com>
2023-03-08 17:32:59 +0100

    Update site with conflicting files from merge

--- org.eclipse.papyrus.arrowhead.deployment/systems/bin/resources/folderGenWin.bat
+++ org.eclipse.papyrus.arrowhead.deployment/systems/bin/resources/folderGenWin.bat
@@ -1,10 +1,11 @@
 C:
 cd C:\Users\usuario\Documents\ltu\2022-support-software-engineer\sysml-plugin-development\workspace\
-mkdir HomeAutomationSystem2_ApplicationSystems
-cd HomeAutomationSystem2_ApplicationSystems
-mkdir controlSystem_Consumer
-mkdir windowOpening_Provider
-mkdir windowMotor_Provider
+mkdir HomeAutomationSystem_ApplicationSystems
+cd HomeAutomationSystem_ApplicationSystems
 mkdir indoorTemp_Provider
+mkdir outdoorTemp_Provider
+mkdir controlSystem_Provider
+mkdir boiler_Consumer
+mkdir iHM_Provider
 
-copy C:\Users\usuario\Documents\ltu\2022-support-software-engineer\sysml-plugin-development\workspace\\arrowhead-papyrus-plugin\org.eclipse.papyrus.arrowhead.deployment\systems\src\resources\.project C:\Users\usuario\Documents\ltu\2022-support-software-engineer\sysml-plugin-development\workspace\\HomeAutomationSystem2_ApplicationSystems
+copy C:\Users\usuario\Documents\ltu\2022-support-software-engineer\sysml-plugin-development\workspace\\arrowhead-papyrus-plugin\org.eclipse.papyrus.arrowhead.deployment\systems\src\resources\.project C:\Users\usuario\Documents\ltu\2022-support-software-engineer\sysml-plugin-development\workspace\\HomeAutomationSystem_ApplicationSystems
--- org.eclipse.papyrus.arrowhead.deployment/systems/bin/resources/indoorTempProviderStructure.bat
+++ org.eclipse.papyrus.arrowhead.deployment/systems/bin/resources/indoorTempProviderStructure.bat
@@ -1,6 +1,6 @@
-%systemroot%\System32\xcopy  C:\Users\usuario\Documents\ltu\2022-support-software-engineer\sysml-plugin-development\workspace\\arrowhead-papyrus-plugin\org.eclipse.papyrus.arrowhead.deployment\systems\src\resources\provider C:\Users\usuario\Documents\ltu\2022-support-software-engineer\sysml-plugin-development\workspace\\HomeAutomationSystem2_ApplicationSystems\indoorTemp_Provider /e
+%systemroot%\System32\xcopy  C:\Users\usuario\Documents\ltu\2022-support-software-engineer\sysml-plugin-development\workspace\\arrowhead-papyrus-plugin\org.eclipse.papyrus.arrowhead.deployment\systems\src\resources\provider C:\Users\usuario\Documents\ltu\2022-support-software-engineer\sysml-plugin-development\workspace\\HomeAutomationSystem_ApplicationSystems\indoorTemp_Provider /e
 C:
-cd C:\Users\usuario\Documents\ltu\2022-support-software-engineer\sysml-plugin-development\workspace\\HomeAutomationSystem2_ApplicationSystems\indoorTemp_Provider\src\main\java\eu\arrowhead
+cd C:\Users\usuario\Documents\ltu\2022-support-software-engineer\sysml-plugin-development\workspace\\HomeAutomationSystem_ApplicationSystems\indoorTemp_Provider\src\main\java\eu\arrowhead
 mkdir indoorTemp_Provider
 mkdir security
 del "C:\Users\usuario\Documents\ltu\2022-support-software-engineer\sysml-plugin-development\workspace\\arrowhead-papyrus-plugin\org.eclipse.papyrus.arrowhead.deployment\systems\src\resources${sysName}ProviderStructure.bat"
